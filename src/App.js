b22d391af422022fa639aa2af89bd59881581715
Andrew Ijano <andrew.lopes@uol.com.br>
2020-10-08 22:04:18 -0300

    Update components to Material UI and add text fields
    Co-authored-by: Pedro Almeida <pedro.hba@hotmail.com>

--- src/App.js
+++ src/App.js
@@ -1,19 +1,86 @@
 import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+import { TextField } from '@material-ui/core';
+
+import ToggleButton from '@material-ui/lab/ToggleButton';
+import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
+
+import TextFieldsIcon from '@material-ui/icons/TextFields';
+import AudiotrackIcon from '@material-ui/icons/Audiotrack';
+import VideocamIcon from '@material-ui/icons/Videocam';
+
+export default () => {
+  const [title, setTitle] = useState(null);
+  const [description, setDescription] = useState(null);
+  const [selectedType, setSelectedType] = useState(null);
   const [selectedFile, setSelectedFile] = useState(null);
 
-  const onFileChange = (event) => {
+  const onFileChange = event => {
     setSelectedFile(URL.createObjectURL(event.target.files[0]));
+  };
+
+  const handleInput = setState => event => {
+    setState(event.target.value);
+  };
+
+  const handleToggle = (event, newType) => {
+    setSelectedType(newType);
   };
 
   return (
     <div className="App">
-      <input type="file" accept="image/*;capture=camera" onChange={onFileChange} />
-      <img src={selectedFile} alt="Selected media by the user." />
+      <h1>Escolha um título para sua história</h1>
+      <TextField
+        id="title-input"
+        value={title}
+        onChange={handleInput(setTitle)}
+      />
+
+      <h1>Insira uma descrição como destaque para sua história</h1>
+      <TextField
+        id="description-input"
+        value={description}
+        onChange={handleInput(setDescription)}
+      />
+
+      <h1>Escolha como você quer contar essa história:</h1>
+      <ToggleButtonGroup
+        size="large"
+        value={selectedType}
+        exclusive
+        onChange={handleToggle}
+      >
+        <ToggleButton value="text">
+          <TextFieldsIcon />
+          Texto
+        </ToggleButton>
+        <ToggleButton value="audio">
+          <AudiotrackIcon />
+          Áudio
+        </ToggleButton>
+        <ToggleButton value="video">
+          <VideocamIcon />
+          Vídeo
+        </ToggleButton>
+      </ToggleButtonGroup>
+
+      <p>
+        {selectedType} {title} {description}{' '}
+      </p>
+
+      <input
+        type="file"
+        accept="image/*;capture=camera"
+        onChange={onFileChange}
+      />
+      {selectedFile && (
+        <img
+          className="App-selected-file"
+          src={selectedFile}
+          alt="Selected media by the user."
+        />
+      )}
     </div>
   );
-}
-
-export default App;
+};
