4a7b8e9e947910e05a2b5d239c121c61f4a54da9
Pedro Almeida <pedro.hba@hotmail.com>
2020-10-20 18:09:11 -0300

    Fix tests of media type selection
    Co-authored-by: Andrew Ijano <andrew.ijano@gmail.com>

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import { shallow } from 'enzyme';
 
 import App from './App';
@@ -11,30 +10,59 @@ describe('App', () => {
     wrapper = shallow(<App />);
   });
 
-  it('should start with an empty title', () => {
-    expect(wrapper.find('#title-input').props().value).toBe('');
+  describe('when it starts', () => {
+    it('should have an empty title', () => {
+      expect(wrapper.find('#title-input').props().value).toBe('');
+    });
+
+    it('should have an empty description', () => {
+      expect(wrapper.find('#description-input').props().value).toBe('');
+    });
+
+    it('should have "text" as selected type', () => {
+      expect(wrapper.find('#media-type-toggle').props().value).toBe('text');
+    });
   });
 
-  it('should start with an empty description', () => {
-    expect(wrapper.find('#description-input').props().value).toBe('');
+  describe('when text type is selected', () => {
+    beforeEach(() => {
+      wrapper.find('#media-type-toggle').props().onChange(null, 'text');
+    });
+
+    it('should render text field', () => {
+      expect(wrapper.exists('#media-text-input')).toBe(true);
+    });
+
+    it('should not render file upload', () => {
+      expect(wrapper.exists('#media-file-upload')).toBe(false);
+    });
   });
 
-  it('should start with "text" as selected type', () => {
-    expect(wrapper.find('#media-type-toggle').props().value).toBe('text');
+  describe('when audio type is selected', () => {
+    beforeEach(() => {
+      wrapper.find('#media-type-toggle').props().onChange(null, 'audio');
+    });
+
+    it('should render file upload', () => {
+      expect(wrapper.exists('#media-file-upload')).toBe(true);
+    });
+
+    it('should not render text field', () => {
+      expect(wrapper.exists('#media-text-input')).toBe(false);
+    });
   });
 
-  it('should render text field when text type is selected', () => {
-    wrapper.find('#media-type-toggle-text').simulate('click');
-    expect(wrapper.exists('#media-text-input')).toBe(true);
-  });
+  describe('when video type is selected', () => {
+    beforeEach(() => {
+      wrapper.find('#media-type-toggle').props().onChange(null, 'video');
+    });
 
-  it('should not render text field when another media type is selected', () => {
-    wrapper.find('#media-type-toggle-audio').simulate('click');
-    expect(wrapper.exists('#media-text-input')).toBe(false);
-  });
+    it('should render file upload', () => {
+      expect(wrapper.exists('#media-file-upload')).toBe(true);
+    });
 
-  it('should render media upload when audio type is selected', () => {
-    wrapper.find('#media-type-toggle-audio').simulate('click');
-    expect(wrapper.exists('#media-file-upload')).toBe(true);
+    it('should not render text field', () => {
+      expect(wrapper.exists('#media-text-input')).toBe(false);
+    });
   });
 });
