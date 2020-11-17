df87750ce2c2818a19967bb985c09eb4ca9fdfca
Andrew Ijano <andrew.lopes@uol.com.br>
2020-11-17 17:19:54 -0300

    Fix git hook issue

--- src/components/sendForm.js
+++ src/components/sendForm.js
@@ -1,36 +1,36 @@
 const getBlobFromLocation = (mediaLocation) =>
-    fetch(mediaLocation).then((response) => response.blob());
+  fetch(mediaLocation).then((response) => response.blob());
 
 const getBlobFromText = (mediaText) =>
-    new Blob([mediaText], { type: 'text/plain' });
+  new Blob([mediaText], { type: "text/plain" });
 
 const getBlob = async ({ media, type }) => {
-    if (type === 'text') return getBlobFromText(media);
-    return getBlobFromLocation(media);
+  if (type === "text") return getBlobFromText(media);
+  return getBlobFromLocation(media);
 };
 
 const getFormData = async (inputData) => {
-    const formData = new FormData();
+  const formData = new FormData();
 
-    const media = await getBlob(inputData);
-    const data = { ...inputData, media };
+  const media = await getBlob(inputData);
+  const data = { ...inputData, media };
 
-    Object.keys(data).forEach((fieldName) => {
-        formData.append(fieldName, data[fieldName]);
-    });
+  Object.keys(data).forEach((fieldName) => {
+    formData.append(fieldName, data[fieldName]);
+  });
 
-    return formData;
+  return formData;
 };
 
 const sendForm = async (inputData) => {
-    const formData = await getFormData(inputData);
+  const formData = await getFormData(inputData);
 
-    const response = await fetch('http://localhost:8000/historia/', {
-        method: 'POST',
-        body: formData,
-    });
+  const response = await fetch("http://localhost:8000/historia/", {
+    method: "POST",
+    body: formData,
+  });
 
-    return response;
+  return response;
 };
 
 export default sendForm;
