ad31911ba871656d223f56ae25189f1691ca2e6b
RenanSchwyz <renancostalaiz@gmail.com>
2020-11-24 22:38:54 -0300

    Add first draft of a multistep form
    Co-authored-by: Andrew Ijano <andrew.ijano@gmail.com>
    Co-authored-by: Artur Magalhães <artur_santos@usp.br>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
-import FormHistoria from "./components/FormHistoria";
+/*import FormHistoria from "./components/FormHistoria";*/
+import MultiStepForm from "./components/MultiStepForm";
 
 function App() {
   return (
     <div className="App">
-      <FormHistoria />
+      {/* <FormHistoria />  */}
+      <MultiStepForm/>
     </div>
   );
 }
