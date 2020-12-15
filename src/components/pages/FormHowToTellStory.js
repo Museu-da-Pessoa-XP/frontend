3db57ba2dbfc2933cd21380f0f4f084230fbb08f
RenanSchwyz <renancostalaiz@gmail.com>
2020-12-14 23:50:41 -0300

    Fix video translation consistency with npx-patch

--- src/components/pages/FormHowToTellStory.js
+++ src/components/pages/FormHowToTellStory.js
@@ -11,11 +11,12 @@ function FormHowToTellStory() {
       <p>
         A sua história deve ter começo, meio e fim! Deve ser uma história
         baseada em fatos reais sobre a sua vivência ou a vivência de outra
-        pessoa que você esteja descrevendo. <br></br><br></br>A história não deve conter
-        apenas opiniões, ela deve ser interessante para quem estiver lendo,
-        escutando ou vendo ela!<br></br><br></br>Lembre-se de ser o mais detalhado
-        possível, quando aconteceu essa história? Onde essa história se passa?
-        Quem são os personagens envolvidos na sua história?
+        pessoa que você esteja descrevendo. <br></br>
+        <br></br>A história não deve conter apenas opiniões, ela deve ser
+        interessante para quem estiver lendo, escutando ou vendo ela!<br></br>
+        <br></br>Lembre-se de ser o mais detalhado possível, quando aconteceu
+        essa história? Onde essa história se passa? Quem são os personagens
+        envolvidos na sua história?
       </p>
     </>
   );
